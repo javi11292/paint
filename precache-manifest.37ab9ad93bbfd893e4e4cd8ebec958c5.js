2e019c971865fe0e9c901061aeef2607cf563a1f
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-09-27 12:43:12 +0200

    Updates

---
+++ precache-manifest.37ab9ad93bbfd893e4e4cd8ebec958c5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8d36fd0f272d3984c592679c8e4993",
+    "url": "/paint/index.html"
+  },
+  {
+    "revision": "7d6c812845be6dc7cf95",
+    "url": "/paint/static/js/2.9e821469.chunk.js"
+  },
+  {
+    "revision": "daa9f44740f5c8fecf69",
+    "url": "/paint/static/js/main.9f0a327e.chunk.js"
+  },
+  {
+    "revision": "dff49a67c4a03452ac68",
+    "url": "/paint/static/js/runtime~main.12e062a5.js"
+  }
+]);
