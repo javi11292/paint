4314efe16c098c1ec5c3c4f42ac359df1a863085
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-09-30 10:30:38 +0200

    Fix instances number

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "server",
         script: "index.js",
         "log_date_format": "DD/MM/YY HH:mm:ss",
-        instances: "max",
+        instances: "1",
         node_args: "-r dotenv/config",
     }, {
         name: "wake",
