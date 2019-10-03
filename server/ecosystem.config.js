9062b6159958f44946006a67e34e3dd420766cef
Javier Robles Fernández <javi_1_12_92@hotmail.com>
2019-10-03 12:37:28 +0200

    Add multicore support

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "server",
         script: "index.js",
         "log_date_format": "DD/MM/YY HH:mm:ss",
-        instances: "1",
+        instances: "max",
         node_args: "-r dotenv/config",
     }, {
         name: "wake",
--- server/src/socket.js
+++ server/src/socket.js
@@ -1,10 +1,11 @@
 "use strict"
 
 const io = require("socket.io")
+const redisAdapter = require("socket.io-redis")
 
 function listen(server) {
     const socketServer = io(server, { origins: "*:*" })
-
+    socketServer.adapter(redisAdapter({ host: "redis", port: 6379 }))
     socketServer.on("connect", socket => {
         socket.on("draw", points => {
             socketServer.emit("draw", points)
